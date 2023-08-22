495fccf12d612a83cdb02a51c84ed15594258b27
Mamy <mamynantenaina2@gmail.com>
2023-08-22 11:19:11 +0300

    add logic Logout

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,16 +1,20 @@
+import { auth } from '@/firebase/firebase';
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Logouts from '../Buttons/Logouts';
 
 type TopBarProps = {
 
 };
 
 const TopBar: React.FC<TopBarProps> = () => {
+    const [user] = useAuthState(auth);
 
     return <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
         <div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
             <Link href='/' className='h-[22px] flex-1'>
-                <img src='/logo-full.png' alt='Logo'className='h-full'/>
+                <img src='/logo-full.png' alt='Logo' className='h-full' />
             </Link>
 
             <div className='flex items-center space-x-4 flex-1 justify-end'>
@@ -24,12 +28,24 @@ const TopBar: React.FC<TopBarProps> = () => {
                         Premium
                     </a>
                 </div>
-                <Link
-                    href='/auth'
-                >
-                    <button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-                </Link>
-
+                {!user && (
+                    <Link
+                        href='/auth'
+                    >
+                        <button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
+                    </Link>
+                )}
+                {user && (
+                    <>
+                        <div className='cursor-pointer group relative'>
+                            <img src="/avatar.png" alt="user profile img" className='h-8 w-8 rounded-full' />
+                            <div className='absolute top-10 left-2/4 -translate-x-2/4 mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0'>
+                                <p className='text-sm'>{user.email}</p>
+                            </div>
+                        </div>
+                        <Logouts />
+                    </>
+                )}
             </div>
         </div>
     </nav>
